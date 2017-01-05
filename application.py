5b07c7c0d9ef77c3729372741b3a2c3048fe21d7
Alex Wong <alexxi98109@gmail.com>
2017-01-04 20:59:13 -0500

    Configuring for deployment

---
+++ application.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from graph import *
+import pylibmc 
+
+application = Flask(__name__)
+mc = pylibmc.Client(["127.0.0.1"],
+		behaviors={"tcp_nodelay": True, "ketama": True})
+
+@application.route('/')
+def home():
+	return render_template("stats.html", stat = "PTS")
+
+@application.route('/', methods = ['POST'])
+def search_player():
+	stat = request.form['stat']
+	names = request.form['player_names']
+	names_split = names.split(", ")
+
+	players = list()
+	for name in names_split:
+		name = name.title()
+		full_name = name.split()
+		pid = str(get_player_id(*full_name))
+	
+		if not (pid is None):
+			if pid in mc:
+				player = mc[pid]
+				players.append(player)
+			else:
+				player = create_player(pid)
+				mc[pid] = player
+				players.append(player)
+		else:
+			return render_template("stats.html", 
+				error = "Player does not exist.")
+
+	stat_graph = generate_line_graph(players, stat)
+	return render_template("stats.html", graph = stat_graph, 
+			player_names = names, stat = stat)
+	
+if __name__ == "__main__":
+	application.run(host='0.0.0.0')
